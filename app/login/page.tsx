ae0c0fc988cdc7ca213ec65063e2f54fdac24ac9
Nine <apichatpong.lert@gmail.com>
2025-10-08 18:10:07 +0700

    import component for login/register page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { auth, provider } from "@/lib/firebaseClient"
+import { signInWithPopup, signInWithEmailAndPassword ,onAuthStateChanged, signOut, User } from "firebase/auth";
+
+
 export default function Pages(){
-    return <>
+    return (
         <div>
             Login Page
         </div>
-    </>
+    )
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { auth } from "@/lib/firebaseClient"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+
+
 export default function Pages(){
-    return <>
+    return (
         <div>
             Register Page
         </div>
-    </>
+    )
 }
