37bcc2fa002aaabf923e83580b68516c5d757295
Kantasnn <santa.kantapon@gmail.com>
2025-10-09 20:28:29 +0700

    Add UX,UI register

--- lib/firebaseClient.tsx
+++ lib/firebaseClient.tsx
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,3 +24,5 @@ const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 
 export { auth, provider };
+export const storage = getStorage(app);
+export default app;
