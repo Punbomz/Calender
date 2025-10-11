94bccdf973dabf582d7919bf98eb22f31818f16f
nine <apichatpong.lert@gmail.com>
2025-10-11 13:19:12 +0700

    fix middleware_2

--- middleware.ts
+++ middleware.ts
@@ -27,14 +27,18 @@ export function middleware(request: NextRequest) {
       return NextResponse.redirect(new URL("/profile", request.url));
     }
   }
+
+  // Redirect to profile if already logged in and trying to access register
   if (pathname === "/register") {
     if (session) {
       console.log("Already logged in, redirecting to profile");
       return NextResponse.redirect(new URL("/profile", request.url));
     }
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/profile/:path*", "/login"],
+  matcher: ["/profile/:path*", "/login", "/register"],
 };
